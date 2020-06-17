bf9e67d250300cffe43b44845ff5cd596079bd46
idesantiagocontreras <idesantiagocontreras@gmail.com>
2020-06-17 13:37:25 -0500

    Variable de entorno de mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB
 if (process.env.NODE_ENV == 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://idesantiago:uxpYOMXgIGqXIwQn@cluster0-ikkfr.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
